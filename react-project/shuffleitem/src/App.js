036269023709b2f137c190538ec0b98a612d054d
Grace Xu <xuweijia222@gmail.com>
2024-12-22 21:05:17 -0800

    add a project which use custom hook - use-force-updat

---
+++ react-project/shuffleitem/src/App.js
@@ -0,0 +1,1 @@
+// Create a React component that displays a list of items and includes a "Shuffle Items" button. Use the useForceUpdate hook to shuffle the order of the items in the list without using React's state management.
